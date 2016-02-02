2b70d188fc8de12b22ca71961ded7022004f570c
swp <sewonpark@hanbit.jp>
2016-02-02 16:45:10 +0900

    page 134

--- site/meadowlark.js
+++ site/meadowlark.js
@@ -8,23 +8,57 @@ var app = express();
 var fortune = require('../lib/fortune.js');
 
 //Handlebars
-var handlebars = require('express-handlebars').create({ defaultLayout : 'main'});
-app.engine('handlebars', handlebars.engine);
-app.set('view engine', 'handlebars');
+var handlebars = require('express-handlebars')
+                    .create(
+                        {
+                            defaultLayout : 'main',
+                            extname: '.hbs',
+                            helpers : {
+                                section: function(name, options){
+                                    if(!this._sections) this._sections = {};
+                                    this._sections[name] = options.fn(this);
+                                    return null;
+                                }
+                            }
+                        }
+                    );
 
-app.set('port', process.env.PORT || 3000);
 
 
 //if(app.thing === null ) console.log('bleat!');
 
+//Mock
+var tours = [
+    { id: 0, name: 'Hood River', price: 99.99 },
+    { id: 1, name: 'Oregon Coast', price: 149.95 }
+];
+
+
 app
+    .engine('hbs', handlebars.engine)
+
+    .set('view engine', 'hbs')
+
+    .set('port', process.env.PORT || 3000)
+
+    .disable('x-powered-by')
 
     //Static Middle Ware
     .use(express.static(__dirname + '/public'))
 
+    //Body-Parser Middle Ware
+    .use(require('body-parser').urlencoded({extended: true}))
+
     //Detect Query String Middle Ware
     .use(function(req, res, next){
         res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
+        next();
+    })
+
+    //Weather Partial Context Inject Middle Ware
+    .use(function(req, res, next){
+        if(!res.locals.partials) res.locals.partials = {};
+        res.locals.partials.weatherContext = getWeatherData();
         next();
     })
 
@@ -62,10 +96,128 @@ app
         res.render('tours/request-group-rate');
     })
 
-    //Custom 404 Page
-    .use(function(req, res){
-        res.status(404);
-        res.render('404');
+    //ErrorPage
+    .get('/error', function(req, res){
+        res
+            .status(500)
+            .render('500');
+    })
+
+    //Parameters
+    .get('/greeting', function(req, res){
+        res.render('about', {
+            message: 'welcome',
+            style: req.query.style,
+            //userid: req.cookie.userid,
+            //username: req.session.username
+        });
+    })
+
+    //No-layout
+    .get('/no-layout', function(req, res){
+        res.render('no-layout', { layout: null });
+    })
+
+    //Custom Layout
+    .get('/custom-layout', function(req, res){
+        res.render('custom-layout', {layout: 'custom'});
+    })
+
+    //Test Text
+    .get('/test', function(req, res){
+        res
+            .type('text/plain')
+            .send('this is a test');
+    })
+
+    //Thank you page
+    .get('/thank-you', function(req, res){
+        res.render('thank-you');
+    })
+
+    //Basic Form Process
+    .post('/process-contact', function(req, res){
+        console.log('Received contact from ' + req.body.name + ' <' + req.body.email + '>');
+        //TODO:: Save To DB
+        res.redirect(303, '/thank-you');
+    })
+
+    //Advance Form Process
+    .post('/process-contact-adv', function(req, res){
+        //TODO :: Must Use Body-Parser Middle Ware
+
+        console.log('Received contact from ' + req.body.name + ' <' + req.body.email + '>');
+
+        try{
+
+            //TODO:: Save To DB
+
+            return res.xhr ? res.render({success: true}) : res.redirect(303, '/thank-you');
+
+        }catch(ex){
+            return res.xhr ? res.render({error : 'DB Error'}) : res.redirect(303, '/db-error');
+        }
+    })
+
+    //Web API - Basic Get
+    .get('/api/tours', function(req, res){
+        res.json(tours);
+    })
+
+    //Web Api - PUT
+    .put('/api/tour/:id', function(req, res){
+        var p = tours.filter(function(p){ return p.id == req.params.id;})[0];
+        if(p){
+            if( req.query.name ) p.name = req.query.name;
+            if( req.query.price ) p.price = req.query.price;
+            res.json({success: true});
+        }else{
+            res.json({error : 'No such tour exists.'});
+        }
+    })
+
+    //Web Api - DEL
+    .del('/api/tour/:id', function(req, res){
+        var i;
+        for(i=tours.length-1; i>=0; i--)
+            if(tours[i].id == req.params.id) break;
+
+        if(i >= 0){
+            tours.splice(i, 1);
+            res.json({success:true});
+        }else{
+            res.json({error:'No such tour exists.'});
+        }
+
+    })
+
+    //Client Template
+    .get('/nursery-rhyme', function(req, res){
+        res.render('nursery-rhyme');
+    })
+
+    //Client Template Data
+    .get('/data/nursery-rhyme', function(req, res){
+        res.json({
+            animal: 'squirrel',
+            bodyPart: 'tail',
+            adjective: 'bushy',
+            noun: 'heck'
+        });
+    })
+
+    //NewsLetter Form
+    .get('/newsletter', function(req, res){
+        //What is CSRF?!
+        res.render('newsletter', { csrf: 'CSRF token goes here' });
+    })
+
+    .post('/process', function(req, res){
+        console.log('Form (from querystring): ' + req.query.form);
+        console.log('CSRF token (from hidden form field): ' + req.body._csrf);
+        console.log('Name (from visible form field): ' + req.body.name);
+        console.log('Email (from visible form field): ' + req.body.email);
+        res.redirect(303, '/thank-you');
     })
 
     //Custom 500 Page
@@ -75,7 +227,43 @@ app
         res.render('500');
     })
 
+    //Custom 404 Page
+    .use(function(req, res){
+        res.status(404);
+        res.render('404');
+    })
+
     //Listen
     .listen(app.get('port'), function(){
        console.log('Express Started on http://localhost:' + app.get('port') + '; press Ctrl + C to terminate.');
     });
+
+
+//Mock Weather Data Get Function
+function getWeatherData(){
+    return{
+      locations:[
+          {
+              name: 'Portland',
+              forecastUrl: 'http://www.wunderground.com/US/OR/Protland.html',
+              iconUrl: 'http://icons-ak.wxug.com/i/c/k/cloudy.gif',
+              weather : 'Overcast',
+              temp : '54.1 F (12.3 C)'
+          },
+          {
+              name: 'Bend',
+              forecastUrl: 'http://www.wunderground.com/US/OR/Bend.html',
+              iconUrl: 'http://icons-ak.wxug.com/i/c/k/partlycloudy.gif',
+              weather : 'Partly Cloudy',
+              temp : '55.0 F (12.8 C)'
+          },
+          {
+              name: 'Manzanita',
+              forecastUrl: 'http://www.wunderground.com/US/OR/Manzanita.html',
+              iconUrl: 'http://icons-ak.wxug.com/i/c/k/rain.gif',
+              weather : 'Light Rain',
+              temp : '55.0 F (12.8 C)'
+          }
+      ]
+    };
+}
